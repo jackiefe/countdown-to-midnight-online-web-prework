20c0ac43ecaa1098c88cbceb9e1af1d5f34e3157
Jacqi E <jackie.entz@gmail.com>
2019-02-07 21:39:28 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
-#write your code here
+require 'pry'
+def countdown(number)
+ while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  # puts "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(x)
+  sleep x
 end
